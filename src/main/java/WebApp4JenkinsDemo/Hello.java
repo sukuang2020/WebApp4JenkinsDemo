41ea7ceb2d76b59735a37d4160160b5cd3f7dd4f
sukuang2020 <sukuang2020@yahoo.co.jp>
2020-08-29 22:21:43 +0900

    test

--- src/main/java/WebApp4JenkinsDemo/Hello.java
+++ src/main/java/WebApp4JenkinsDemo/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/hello")
     public String sayHello(){
     	//ss 修改返回的字符串的内容
-        return "Hello2:" + new Date();
+        return "Hello7:" + new Date();
     }
 }
